9ea5d13c5e59a5f4d2605d50721c840da8461c57
JohankoG <jan.gottweis@trojsten.sk>
2024-11-12 22:32:56 +0100

    uvodny commit

---
+++ src/main/java/sk/uniba/fmph/dcs/Location.java
@@ -0,0 +1,20 @@
+package sk.uniba.fmph.dcs;
+
+public enum Location {
+    ToolMaker,
+    Hut,
+    Field,
+    HuntingGrounds,
+    Forest,
+    ClayMound,
+    Quary,
+    River,
+    CivilizationCard1,
+    CivilizationCard2,
+    CivilizationCard3,
+    CivilizationCard4,
+    BuildingTile1,
+    BuildingTile2,
+    BuildingTile3,
+    BuildingTile4,
+}
