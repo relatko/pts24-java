7763fbd23eee0023e94339ed0881c71d590d40a5
unknown <ldmlkvcv@gmail.com>
2024-11-13 10:46:51 +0100

    GameEndStateTest

--- src/test/java/sk/uniba/fmph/dcs/game_phase_controller/GameEndStateTest.java
+++ src/test/java/sk/uniba/fmph/dcs/game_phase_controller/GameEndStateTest.java
@@ -1,8 +1,10 @@
 package sk.uniba.fmph.dcs.game_phase_controller;
 
 import org.junit.Test;
-import sk.uniba.fmph.dcs.stone_age.HasAction;
-import sk.uniba.fmph.dcs.stone_age.PlayerOrder;
+import sk.uniba.fmph.dcs.stone_age.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 import static org.junit.Assert.assertEquals;
 
@@ -14,5 +16,25 @@ public class GameEndStateTest {
         int players = 1;
         PlayerOrder player = new PlayerOrder(order, players);
         assertEquals(gameEndState.tryToMakeAutomaticAction(player), HasAction.WAITING_FOR_PLAYER_ACTION);
+    }
+
+    @Test
+    public void test_methodsReturnActionFailure() {
+        int order = 1;
+        int players = 1;
+        PlayerOrder player = new PlayerOrder(order, players);
+
+        Collection<Effect> resources = new ArrayList<>();
+
+        GameEndState gameEndState = new GameEndState();
+
+        assertEquals(gameEndState.doNotFeedThisTurn(player), ActionResult.FAILURE);
+        assertEquals(gameEndState.feedTribe(player, resources), ActionResult.FAILURE);
+        assertEquals(gameEndState.makeAction(player, Location.BUILDING_TILE1, resources, resources), ActionResult.FAILURE);
+        assertEquals(gameEndState.skipAction(player, Location.BUILDING_TILE1), ActionResult.FAILURE);
+        assertEquals(gameEndState.makeAllPlayersTakeARewardChoice(player, Effect.WOOD), ActionResult.FAILURE);
+        assertEquals(gameEndState.placeFigures(player, Location.BUILDING_TILE1, 1), ActionResult.FAILURE);
+        assertEquals(gameEndState.noMoreToolsThisThrow(player), ActionResult.FAILURE);
+        assertEquals(gameEndState.useTools(player, 0), ActionResult.FAILURE);
     }
 }
