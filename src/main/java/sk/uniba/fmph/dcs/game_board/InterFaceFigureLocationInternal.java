3f9bdfb9752a9eda151478d38994a827bbf2a554
Jakubino158 <dzacovskyjakub@gmail.com>
2024-11-17 09:28:20 +0100

    bugfix: class to interface InterFaceFigureLocationInternal

--- src/main/java/sk/uniba/fmph/dcs/game_board/InterFaceFigureLocationInternal.java
+++ src/main/java/sk/uniba/fmph/dcs/game_board/InterFaceFigureLocationInternal.java
@@ -4,8 +4,8 @@ import sk.uniba.fmph.dcs.stone_age.ActionResult;
 import sk.uniba.fmph.dcs.stone_age.Effect;
 import sk.uniba.fmph.dcs.stone_age.HasAction;
 
-public class InterFaceFigureLocationInternal {
-    placeFigures(Player player, int figureCount);
+public interface InterFaceFigureLocationInternal {
+    boolean placeFigures (Player player, int figureCount);
     HasAction tryToPlaceFigures(Player player, int count);
     ActionResult makeAction(Player player, Effect[] inputResources, Effect[] outputResources);
     boolean skipAction(Player player);
