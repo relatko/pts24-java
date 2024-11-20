95604c9d1ec8faec1e2a58ccc7cd4a491a516698
Jakubino158 <dzacovskyjakub@gmail.com>
2024-11-20 11:54:21 +0100

    chore: unify assertions import

--- src/test/java/sk/uniba/fmph/dcs/game_board/SimpleBuildingTest.java
+++ src/test/java/sk/uniba/fmph/dcs/game_board/SimpleBuildingTest.java
@@ -3,9 +3,10 @@ package sk.uniba.fmph.dcs.game_board;
 import java.util.ArrayList;
 import java.util.OptionalInt;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import sk.uniba.fmph.dcs.stone_age.Effect;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SimpleBuildingTest {
     @Test
@@ -15,11 +16,11 @@ public class SimpleBuildingTest {
 
         SimpleBuilding building = new SimpleBuilding(buildingResources);
 
-        Assertions.assertEquals(building.build(buildingResources), OptionalInt.of(3));
+        assertEquals(building.build(buildingResources), OptionalInt.of(3));
 
         ArrayList<Effect> otherResources = new ArrayList<Effect>();
         otherResources.add(Effect.WOOD);
         otherResources.add(Effect.WOOD);
-        Assertions.assertEquals(building.build(otherResources), OptionalInt.empty());
+        assertEquals(building.build(otherResources), OptionalInt.empty());
     }
 }
