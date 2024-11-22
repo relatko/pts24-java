dba3932d0e1cb8545add6e38348424965ba96744
Thohmass <115932241+Thohmass@users.noreply.github.com>
2024-11-22 13:12:29 +0100

    chore: fix lint error in class Throw

--- src/main/java/sk/uniba/fmph/dcs/game_board/Throw.java
+++ src/main/java/sk/uniba/fmph/dcs/game_board/Throw.java
@@ -1,10 +1,12 @@
 package sk.uniba.fmph.dcs.game_board;
 
 public final class Throw {
-    public int[] ThrowDice(int dice) {
-        int [] results = new int[dice];
+
+    public int[] throwDice (final int dice) {
+        int[] results = new int[dice];
         for (int i = 0; i < results.length; i++) {
-            results[i] = (int) (Math.random() * 6) + 1;
+            int dieFaces = 6;
+            results[i] = (int) (Math.random() * dieFaces) + 1;
         }
         return results;
     }
