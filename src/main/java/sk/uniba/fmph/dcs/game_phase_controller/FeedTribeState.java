6ea14ee053a726392f91ab424bec07719dbd646a
tomik-sys <pauliskotomas@gmail.com>
2024-11-29 09:20:04 -0500

    feat AllPlayersTakeARewardState feat FeedTribeState

--- src/main/java/sk/uniba/fmph/dcs/game_phase_controller/FeedTribeState.java
+++ src/main/java/sk/uniba/fmph/dcs/game_phase_controller/FeedTribeState.java
@@ -1,70 +1,76 @@
 package sk.uniba.fmph.dcs.game_phase_controller;
 
-import sk.uniba.fmph.dcs.stone_age.*;
+import sk.uniba.fmph.dcs.stone_age.ActionResult;
+import sk.uniba.fmph.dcs.stone_age.Effect;
+import sk.uniba.fmph.dcs.stone_age.HasAction;
+import sk.uniba.fmph.dcs.stone_age.Location;
+import sk.uniba.fmph.dcs.stone_age.PlayerOrder;
+import sk.uniba.fmph.dcs.stone_age.InterfaceFeedTribe;
 
 import java.util.Collection;
 import java.util.Map;
 
-public final class  FeedTribeState implements InterfaceGamePhaseState{
+public final class FeedTribeState implements InterfaceGamePhaseState {
 
     private final Map<PlayerOrder, InterfaceFeedTribe> interfaceFeedTribeCollection;
 
-    public FeedTribeState(Map<PlayerOrder, InterfaceFeedTribe> interfaceFeedTribeCollection) {
+    public FeedTribeState(final Map<PlayerOrder, InterfaceFeedTribe>  interfaceFeedTribeCollection) {
         this.interfaceFeedTribeCollection = interfaceFeedTribeCollection;
     }
 
     @Override
-    public ActionResult placeFigures(PlayerOrder player, Location location, int figuresCount) {
+    public ActionResult placeFigures(final PlayerOrder player, final Location location, final int figuresCount) {
         return ActionResult.FAILURE;
     }
 
     @Override
-    public ActionResult makeAction(PlayerOrder player, Location location, Collection<Effect> inputResources, Collection<Effect> outputResources) {
+    public ActionResult makeAction(final PlayerOrder player, final Location location,
+            final Collection<Effect> inputResources, final Collection<Effect> outputResources) {
         return ActionResult.FAILURE;
     }
 
     @Override
-    public ActionResult skipAction(PlayerOrder player, Location location) {
+    public ActionResult skipAction(final PlayerOrder player, final Location location) {
         return ActionResult.FAILURE;
     }
 
     @Override
-    public ActionResult useTools(PlayerOrder player, int toolIndex) {
+    public ActionResult useTools(final PlayerOrder player, final int toolIndex) {
         return ActionResult.FAILURE;
     }
 
     @Override
-    public ActionResult noMoreToolsThisThrow(PlayerOrder player) {
+    public ActionResult noMoreToolsThisThrow(final PlayerOrder player) {
         return ActionResult.FAILURE;
     }
 
     @Override
-    public ActionResult feedTribe(PlayerOrder player, final Collection<Effect> resources) {
-        if(interfaceFeedTribeCollection.get(player).feedTribe(resources)) {
+    public ActionResult feedTribe(final PlayerOrder player, final Collection<Effect> resources) {
+        if (interfaceFeedTribeCollection.get(player).feedTribe(resources)) {
             return ActionResult.ACTION_DONE;
         }
         return ActionResult.FAILURE;
     }
 
     @Override
-    public ActionResult doNotFeedThisTurn(PlayerOrder player) {
-        if(interfaceFeedTribeCollection.get(player).doNotFeedThisTurn()) {
+    public ActionResult doNotFeedThisTurn(final PlayerOrder player) {
+        if (interfaceFeedTribeCollection.get(player).doNotFeedThisTurn()) {
             return ActionResult.ACTION_DONE;
         }
         return ActionResult.FAILURE;
     }
 
     @Override
-    public ActionResult makeAllPlayersTakeARewardChoice(PlayerOrder player, Effect reward) {
+    public ActionResult makeAllPlayersTakeARewardChoice(final PlayerOrder player, final Effect reward) {
         return ActionResult.FAILURE;
     }
 
     @Override
-    public HasAction tryToMakeAutomaticAction(PlayerOrder player) {
-        if(interfaceFeedTribeCollection.get(player).isTribeFed()) {
+    public HasAction tryToMakeAutomaticAction(final PlayerOrder player) {
+        if (interfaceFeedTribeCollection.get(player).isTribeFed()) {
             return HasAction.NO_ACTION_POSSIBLE;
         }
-        if(interfaceFeedTribeCollection.get(player).feedTribeIfEnoughFood()) {
+        if (interfaceFeedTribeCollection.get(player).feedTribeIfEnoughFood()) {
             return HasAction.AUTOMATIC_ACTION_DONE;
         }
         return HasAction.WAITING_FOR_PLAYER_ACTION;
